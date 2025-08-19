30a5f8df7d38881a263500d46ebb6847cf656329
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2025-08-19 00:32:21 -0400

    Update abrir_ticket.bat

--- abrir_ticket.bat
+++ abrir_ticket.bat
@@ -5,7 +5,7 @@ set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
 REM URL de la página que deseas abrir
 set URL=https://admiopro.roanexus.com/public/print-ticket-screen.php
 
-REM Ejecutar Chrome en modo kiosco
-start "" %CHROME_PATH% --kiosk %URL%
+REM Ejecutar Chrome en modo kiosco optimizado
+start "" %CHROME_PATH% --kiosk %URL% --no-first-run --disable-infobars --disable-session-crashed-bubble --disable-translate --disable-notifications --disable-extensions --disable-plugins --disable-background-networking --disable-sync --disable-component-update --disable-default-apps --disable-popup-blocking --disable-features=TranslateUI
 
 exit
